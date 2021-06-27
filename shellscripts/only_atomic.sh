906562859dc7d6d20bbf5949604cb68b6e623fc0
Daniel2497 <73161050+Daniel2497@users.noreply.github.com>
2021-06-27 13:32:52 +0200

    Update only_atomic.sh

--- shellscripts/only_atomic.sh
+++ shellscripts/only_atomic.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-touch allsharedresults
+touch onlyatomicresults
 for ((config=1; config<=3;config++))
 	do
-	echo "Cachekonfiguration" $config>>allsharedresults
+	echo "Cachekonfiguration" $config>>onlyatomicresults
 	for (( sx=4; sx<=64; sx*=2 )) #Ausführung für i=1,2,...,16
     		do
-    		echo "sx=" $sx ", sy=16">>allsharedresults
+    		echo "sx=" $sx ", sy=16">>onlyatomicresults
     		for ((i=1;i<=16;i*=2))
     			do
-    			echo "i="$i >> allsharedresults
-    			./onlyshared $sx 16 $i $config | grep -i [C] >>allsharedresults
-    			echo >> allsharedresults
+    			echo "i="$i >> onlyatomicresults
+    			./onlyatomic $sx 16 $i $config | grep -i [C] >>onlyatomicresults
+    			echo >> onlyatomicresults
     		done
-    		echo >> allsharedresults
+    		echo >> onlyatomicresults
 	done
 done
