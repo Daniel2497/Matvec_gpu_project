97af9e6fc8881af981fe5a2c64430e98474770a7
Daniel2497 <73161050+Daniel2497@users.noreply.github.com>
2021-06-27 13:34:00 +0200

    Update shared_atomic.sh

--- shellscripts/shared_atomic.sh
+++ shellscripts/shared_atomic.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-touch allsharedresults
+touch sharedatomicresults
 for ((config=1; config<=3;config++))
 	do
-	echo "Cachekonfiguration" $config>>allsharedresults
+	echo "Cachekonfiguration" $config>>sharedatomicresults
 	for (( sx=4; sx<=64; sx*=2 )) #Ausführung für i=1,2,...,16
     		do
-    		echo "sx=" $sx ", sy=16">>allsharedresults
+    		echo "sx=" $sx ", sy=16">>sharedatomicresults
     		for ((i=1;i<=16;i*=2))
     			do
-    			echo "i="$i >> allsharedresults
-    			./onlyshared $sx 16 $i $config | grep -i [C] >>allsharedresults
-    			echo >> allsharedresults
+    			echo "i="$i >> sharedatomicresults
+    			./onlyshared $sx 16 $i $config | grep -i [C] >>sharedatomicresults
+    			echo >> sharedatomicresults
     		done
-    		echo >> allsharedresults
+    		echo >> sharedatomicresults
 	done
 done
