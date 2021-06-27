5164621121fea7686f36be9989d2cec85d7407e2
Daniel Emil Giring <daniel.giring@uni-jena.de>
2021-06-27 12:01:39 +0200

    Cache Config Argument

--- shellscripts/allshared.sh
+++ shellscripts/allshared.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 touch allsharedresults
-for (( sx=4; sx<=64; sx*=2 )) #Ausführung für i=1,2,...,16
-    do
-    echo "sx=" $sx ", sy=16">>allsharedresults
-    for ((i=1;i<=16;i*=2))
-    	do
-    	echo "i="$i >> allsharedresults
-    	./onlyshared $sx 16 $i | grep -i [C] >>allsharedresults
-    	echo >> allsharedresults
-    done
-    echo >> allsharedresults
+for((config=1; config<=3;config++))
+	for (( sx=4; sx<=64; sx*=2 )) #Ausführung für i=1,2,...,16
+    		do
+    		echo "sx=" $sx ", sy=16">>allsharedresults
+    		for ((i=1;i<=16;i*=2))
+    			do
+    			echo "i="$i >> allsharedresults
+    			./onlyshared $sx 16 $i $config | grep -i [C] >>allsharedresults
+    			echo >> allsharedresults
+    		done
+    		echo >> allsharedresults
+	done
 done
