81312416b8304620f8c357fef9ae7a940cc9797e
Daniel Emil Giring <daniel.giring@yahoo.de>
2021-06-27 11:57:04 +0200

    Shellscriptordner

---
+++ shellscripts/allshared.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+touch allsharedresults
+for (( sx=4; sx<=64; sx*=2 )) #Ausführung für i=1,2,...,16
+    do
+    echo "sx=" $sx ", sy=16">>allsharedresults
+    for ((i=1;i<=16;i*=2))
+    	do
+    	echo "i="$i >> allsharedresults
+    	./onlyshared $sx 16 $i | grep -i [C] >>allsharedresults
+    	echo >> allsharedresults
+    done
+    echo >> allsharedresults
+done
