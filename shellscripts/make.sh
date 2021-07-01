a1bf9f0f6ddaaf01e18e1017cc3cecfaf0d5f481
Daniel Emil Giring <daniel.giring@yahoo.de>
2021-07-01 22:03:44 +0200

    Neue Intragridmessung

--- shellscripts/make.sh
+++ shellscripts/make.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 nvcc -arch=sm_35 ../Cuda-Scripte/intragrid.cu -o intragrid
 nvcc -arch=sm_35 ../Cuda-Scripte/mullshuffle.cu -o shuffle
-nvcc -arch=sm_35 ../Cuda-Scripte/only_atomic_time_messure.cu -o onlyatomics
+nvcc -arch=sm_35 ../Cuda-Scripte/only_atomic_time_messure.cu -o onlyatomic
 nvcc -arch=sm_35 ../Cuda-Scripte/shared_with_atomic_time.cu -o sharedatomic
 nvcc -arch=sm_35 ../Cuda-Scripte/onlyshared.cu -o onlyshared
 
