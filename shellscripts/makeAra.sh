dd54809a2e6bcbcc09e2467b3dae2b8096a7d7a0
Daniel2497 <da67jod@login01.ara>
2021-07-01 22:30:23 +0200

    P100 Shuffle Messungen

--- shellscripts/makeAra.sh
+++ shellscripts/makeAra.sh
@@ -4,7 +4,7 @@ module load nvidia/cuda/11.3
 
 nvcc -arch=sm_60 ../Cuda-Scripte/intragrid.cu -o intragrid
 nvcc -arch=sm_60 ../Cuda-Scripte/mullshuffle.cu -o shuffle
-nvcc -arch=sm_60 ../Cuda-Scripte/only_atomic_time_messure.cu -o onlyatomics
+nvcc -arch=sm_60 ../Cuda-Scripte/only_atomic_time_messure.cu -o onlyatomic
 nvcc -arch=sm_60 ../Cuda-Scripte/shared_with_atomic_time.cu -o sharedatomic
 nvcc -arch=sm_60 ../Cuda-Scripte/onlyshared.cu -o onlyshared
 
--- shellscripts/sbatch_shuffle.sh
+++ shellscripts/sbatch_shuffle.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=matmul-blocked-benchmark
-#SBATCH --partition=gpu_v100
+#SBATCH --partition=gpu_p100
 #SBATCH --nodes=1
 #SBATCH --mem=16GB
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks=1
 #SBATCH --time=00:05:00
-./shuffle 32 32 16 2
+./shuffle.sh
